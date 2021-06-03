7a7c9803a1de0e75a8df321f70660fa22b38990e
AleksandarPaunov <67508193+AleksandarPaunov@users.noreply.github.com>
2021-06-04 00:03:26 +0300

    Add project files.

---
+++ MovieStarTask1/MovieStar.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MovieStarTask1
+{
+    public class MovieStar
+    {
+        public string DateOfBirth { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string Sex { get; set; }
+        public string Nationality { get; set; }
+
+
+      
+    }
+}
---
+++ MovieStarTask1/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using Newtonsoft.Json;
+
+
+
+namespace MovieStarTask1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+
+
+            var jsonText = File.ReadAllText("input.txt"); 
+            var movieStars = JsonConvert.DeserializeObject<IList<MovieStar>>(jsonText);
+
+            foreach (var star in movieStars)
+            {
+                Console.WriteLine(star.Name +" "+star.Surname);
+                Console.WriteLine(star.Sex);
+                Console.WriteLine(star.Nationality);
+                
+                //Console.WriteLine(DateTime.Now.Year-DateTime.Parse(star.DateOfBirth).Year+" years old");
+                //Age(star.DateOfBirth);
+                AgeCalculation2(star.DateOfBirth);
+            }
+
+        }
+        public static void Age(string dateOfBirth) // Easy way to get the age but not as accurate.
+        {
+            var customerBY = DateTime.Parse(dateOfBirth).Year;
+            var today = DateTime.Now.Year;
+
+            var age = today - customerBY;
+
+            Console.WriteLine(age + " years old");
+
+        }
+
+        public static void AgeCalculation2(string dateOfBirth) // More accurage but uglier way.
+        {
+            var dobApi = DateTime.Parse(dateOfBirth);
+            int now = int.Parse(DateTime.Now.ToString("yyyyMMdd"));
+            int dob = int.Parse(dobApi.ToString("yyyyMMdd"));
+            int age = (now - dob) / 10000; // drops the last 4 digits
+
+            Console.WriteLine(age + " years old");
+
+        }
+    }
+}
